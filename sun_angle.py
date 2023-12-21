fb6c7c08a2be9cf3df125688ba138da74e6d1bb4
andynic <andysnic@hotmail.com>
2023-12-21 19:54:50 +0200

    small corrections and comments

--- sun_angle.py
+++ sun_angle.py
@@ -27,31 +27,24 @@ def sunae(year, day, hour, lat, long):
     # get the current julian date (actually add 2,400,000 for input parameters jd)
     delta = year - 1949.0
     leap = int(delta / 4.0)
-    jd = 32916.5 + delta * 365. + leap - day + hour / 24
+    jd = 32916.5 + delta * 365. + leap + day + hour / 24  # 1st no. is mid. 0 jan 1949 minus 2.4e6: leap = leap days since 1949
 
-    # 1st no. is mid. 0 jan 1949 minus 2.4e6: leap = leap days since 1949
     # calculate ecliptic coordinates
-    time = jd - 51545.0
-    # 51545.0 + 2.4e6 = noon I jan 2000
+    time = jd - 51545.0  # 51545.0 + 2.4e6 = noon 1 jan 2000
 
-    # force mean longitude between 0 and 360 degs
+
     mnlong = 280.460 + 0.9856474 * time
-    mnlong = mnlong % 360.0
-    if mnlong < 0.0:
-        mnlong += 360.0
+    mnlong = mnlong % 360.0  # force mean longitude between 0 and 360 degs
 
     # mean anomaly in radians between 0 and 2*pi
     mnanom = 357.528 + 0.9856003 * time
     mnanom = mnanom % 360.0
-    if mnanom < 0.0:
-        mnanom += 360.0
     mnanom = mnanom * rad
 
     # compute ecliptic longitude and obliquity of ecliptic in radians
     eclong = mnlong + 1.915 * mnanom + 0.20 * math.sin(2.0 * mnanom)
     eclong = eclong % 360.0
-    if eclong < 0.0:
-        eclong += 360.0
+
     oblqec = 23.429 - 0.000004 * time
     eclong = eclong * rad
     oblqec = oblqec * rad
@@ -82,34 +75,50 @@ def sunae(year, day, hour, lat, long):
     if lmst < 0.0:
         lmst += 24.0
     lmst = lmst * 15.0 * rad
+
+    # calculate hour angle in radians between -pi and pi
     ha = lmst - ra
     if ha < -pi:
         ha += twopi
     elif ha > pi:
         ha -= twopi
+
+    # change latitude to radians
     lat = lat * rad
+
+    # calculate azimuth and elevation
     el = math.asin(math.sin(dec) * math.sin(lat) + math.cos(dec) * math.cos(lat) * math.cos(ha))
     az = math.asin(-math.cos(dec) * math.sin(ha) / math.cos(el))
+
+    # if az = 90 degs. el critical = asin(sin(dec)/sin(lat))
     elc = math.asin(math.sin(dec) / math.sin(lat))
     if el >= elc:
         az = pi - az
     elif el <= elc and ha > 0.0:
         az += twopi
+    # this puts azimuth between 0 and 2*pi radians
+
+    # calculate refraction correction for US stan. atmosphere c need to have el in degs before calculating correction
     el = el / rad
     if el > -0.56:
         refrac = 3.51561 * (0.1594 + 0.0196 * el + 0.00002 * el ** 2) / (1.0 + 0.505 * el + 0.0845 * el ** 2)
     else:
         refrac = 0.56
+    # note that 3.51561 = 1013.2 mb/288.2 C
     el += refrac
+    # elevation in degs
+
+    # convert az and lat to degs before returning
     az = az / rad
     lat = lat / rad
+    # mnlong in degs, gmst in hours, jd in days if 2.4e6 added; mnanom, eclong, oblqec, ra, dec, Imst. and ha in radians
     return az, el
 
 
 if __name__ == "__main__":
     # Example usage:
     year = 2023
-    day = 353 # 19 dec
+    day = 353  # 19 dec
     hour = 23.75
     lat = 45.627031343394115  # Latitude in degrees
     long = 25.489147381233582  # Longitude in degrees
